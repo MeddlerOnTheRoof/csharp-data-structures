4af4c3563567a55cb9eeb14b41cab8e495fc0a95
MeddlerOnTheRoof <schanne.mj@gmail.com>
2019-07-23 00:08:21 -0400

    dotnet new console && dotnet restore && dotnet run

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_data_structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
